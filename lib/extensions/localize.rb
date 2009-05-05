fded09d0e3f915b9ec65ee9abddb3dcc139e7292
Jorgen H. Fjeld <jorgen@veridit.no>
2009-05-05 15:01:27 +0200

    Added workaround such that translation strings that raise a I18n error do not raise an exception. The original text is used instead.

--- lib/extensions/localize.rb
+++ lib/extensions/localize.rb
@@ -1,8 +1,11 @@
 class Object
   def as_(key, options = {})
     unless key.blank?
-      text = I18n.translate "#{key}", {:scope => [:active_scaffold], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
-      # text = nil if text.include?('translation missing:')
+      text = begin
+               I18n.translate "#{key}", {:scope => [:active_scaffold], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
+               # text = nil if text.include?('translation missing:')
+             rescue 
+             end
     end
     text ||= key 
     text
